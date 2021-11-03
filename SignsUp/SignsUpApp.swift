92ed874d1e0c3a5e4e5d5c562cfa5c03567505b7
Markim Shaw <ms79723@gmail.com>
2021-11-03 00:31:27 -0500

    Initial Commit

---
+++ SignsUp/SignsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignsUpApp.swift
+//  SignsUp
+//
+//  Created by Markim Shaw on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SignsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
