b0b4ab0a93d942edd3e52764e5688512cc6d9843
Markim Shaw <ms79723@gmail.com>
2021-11-15 15:52:07 -0600

    Create MLGameStateStack file

---
+++ ocky/MLGameStateStack.swift
@@ -0,0 +1,47 @@
+//
+//  MLGameStateStack.swift
+//  ocky
+//
+//  Created by Markim Shaw on 11/15/21.
+//
+
+import Foundation
+
+/// A class that manages a LIFO stack of `MLGameState` objects
+class MLGameStateStack {
+  private(set) var stack: [MLGameState]
+  
+  init() {
+    stack = []
+  }
+  
+  /// Push a new `MLGameState` object on top of the LIFO stack. This function also maintains
+  /// the flow of the `MLGame`.
+  /// This object **will not** push `.inQuestion` objects on top of each other. It will simply replace those.
+  /// This object also does not push the following states: `.findMatch`, `.loadMatch`, `.loadMatches`, `.result`.
+  /// - Parameter state: The `MLGameState` object to push.
+  func push(_ state: MLGameState) {
+    if case .inQuestion = self.peek(),
+       case .inQuestion = state {
+      self.pop()
+    }
+    
+    switch state {
+      case .findMatch, .loadMatch, .loadMatches, .result:
+        return
+      default: stack.append(state)
+    }
+  }
+  
+  /// Removes the most recent `MLGameState` from the stack. It will auto push `.idle` onto the stack if there are
+  /// no other items in the stack. This guarantees that the stack *always has at least one* state in the stack.
+  func pop() {
+    stack = stack.dropLast()
+    stack.count == 0 ? push(.idle) : ()
+  }
+  
+  /// Returns the last object in the stack.
+  func peek() -> MLGameState? {
+    stack.last
+  }
+}
--- ocky/Observables/MLGame.swift
+++ ocky/Observables/MLGame.swift
@@ -10,37 +10,6 @@ import SwiftUI
 import GameKit
 import Combine
 
-class MLGameStateStack {
-  var stack: [MLGameState]
-  
-  init() {
-    stack = []
-  }
-  
-  func push(_ state: MLGameState) {
-    if case .inQuestion = self.peek(),
-       case .inQuestion = state {
-      self.pop()
-    }
-    
-    switch state {
-      case .findMatch, .loadMatch, .loadMatches, .result:
-        return
-      default: stack.append(state)
-    }
-  }
-  
-  func pop() {
-    stack = stack.dropLast()
-    stack.count == 0 ? push(.idle) : ()
-  }
-  
-  func peek() -> MLGameState? {
-    stack.last
-  }
-}
-
-
 class MLGame: NSObject, ObservableObject {
   var gameData: MLGameData
   lazy var stateStack: MLGameStateStack = MLGameStateStack()
@@ -153,6 +122,7 @@ class MLGame: NSObject, ObservableObject {
     guard let data = gameData.encode() else { return }
     do {
       if activeMatch?.currentParticipant?.player?.displayName == GKLocalPlayer.local.displayName {
+        // filter out the user info
         try await activeMatch?.participantQuitInTurn(with: .quit,
                                                      nextParticipants: activeMatch?.participants ?? [],
                                                      turnTimeout: GKTurnTimeoutDefault, match: data)
